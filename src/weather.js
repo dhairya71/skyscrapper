644ab9e6d18da3c90e88b3815ae59afaa58af8fa
dhairya71 <dabi1998dhairya@gmail.com>
2020-10-22 12:03:58 +0530

    updated

---
+++ src/weather.js
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+import "./weather.css";
+function Weather() {
+  return (
+    <div className="main">
+      <h1>Welcome to Dogspoint!!</h1>
+    </div>
+  );
+}
+
+export default Weather;
